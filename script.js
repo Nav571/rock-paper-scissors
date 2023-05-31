8bdb92f5ab445173f1c5490950578bd018289c4a
nav571 <128248831+Nav571@users.noreply.github.com>
2023-05-31 14:57:40 -0400

    Added selectors and made deletions to current code

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-//*Script for rock paper scissors*//
-console.log("Do you wanna play a game?");
-let playerScore = 0
-let computerScore = 0
-roundsPlayed = 0
+//JavaScript    Completed
+const remark= document.querySelector('.remark');
+const roundScore= document.querySelector('.roundScore');
+const btn= document.querySelectorAll('.btn');
+const player= document.getElementById('player-score');
+const computer= document.getElementById('computer-score');
+const roundplay=document.getElementById('round');
+const record= document.querySelector('.record');
+const restart= document.querySelector('.restart');
+const endResult= document.querySelector('.endResult');
+const weapons= document.querySelector('.weapons');
+const instruction= document.querySelector('.instruction');
+
+let playerScore= 0;
+let computerScore=0;
+let roundsPlayed= 0;
 
 /*Random choice by the computer*/
 
@@ -10,7 +21,7 @@ function getComputerchoice() {
     let computerChoice = ["rock", "paper", "scissors"];
     return computerChoice [Math.floor(Math.random()*computerChoice.length)];
 }
- 
+
 /* One round of game*/
 
 function playRound(playerSelection, computerSelection) {
@@ -38,28 +49,15 @@ function playRound(playerSelection, computerSelection) {
     } else {
         return ("its broken, you broke it...or you mistyped! try again");
     }  
-           
+
 }
 
-function game() {
-    console.log("Best of 5, lets gooo!");
-    for (let i=0; i<5; i++) {
-        let playerSelection = prompt("Choose your weapon! rock, paper or scissors", "rock paper scissors").toLowerCase();
-        let computerSelection = getComputerchoice()
-        console.log(playRound(playerSelection, computerSelection))
-        let score = `Round ${roundsPlayed} (Computer ${computerScore} - User ${playerScore})`
-        console.log(score)
-    }
-    if (playerScore > computerScore) {
-        return (`Game over. You Win! Good job! 5 stars for you! (Computer ${computerScore} - User ${playerScore})`)
-    } else if (playerScore < computerScore) {
-        return(`Game over, You lose! Better luck next time! (Computer ${computerScore} - User ${playerScore})`)
-    } else {
-        return (`tied. Good job, better than losing I guess. (Computer ${computerScore} - User ${playerScore})`)
-    }
-}
-console.log(game())
-console.log("Good game, refresh to play again!")
+
+
+
+
+
+
 
       
         
